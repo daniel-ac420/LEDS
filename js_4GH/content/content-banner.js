f39ebbf51ab22dea292a47ddd2c8fa55e31c44ae
daniel-ac420 <67051360+daniel-ac420@users.noreply.github.com>
2026-02-03 19:51:50 +0500

    Delete js_4GH directory

--- js_4GH/content/content-banner.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const items = document.querySelectorAll(".section--banner-content-page .reasons__item");
-	const itemCount = items.length / 2; /* с учётом элементов в .reasons__items.hidden */
-
-	document.querySelector(".section--banner-content-page").style.setProperty("--items-count", itemCount);
-});
